3a03fd2a35aae0faae47d13ca4d62916ff3c3eeb
RomaOkorosso <okorosso.roma.work@gmail.com>
2021-07-18 03:11:01 +1000

    FB-18 fix Roma`s id

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # FeedbackBot
 # Created by romanesin on 16.07.2021
 
-admins = [374085219, 520526643, 440740323]
+admins = [374085219, 440740323]
 admin_chats = [-1001406890577, -1001393887157]
 
 # Token for local testing
